2c7da7d0249eb63c1482c825272a5ea181ed6dc1
Dev Kumar Bachchan <dkbachchan@myseneca.ca>
2025-02-06 15:06:08 -0500

    Add initial data files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect('/about');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/about.html'));
+});
+
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+  });
+
+
+app.listen(HTTP_PORT, () => {
+  console.log('Express http server listening on port ' + HTTP_PORT);
+});
